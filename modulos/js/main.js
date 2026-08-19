f878369b21197655de8a4ae22334a9d53d8946c9
Rodrigo Alexandre <rodrigo11.vgp@gmail.com>
2026-08-18 21:33:14 -0300

    feat: add modules

---
+++ modulos/js/main.js
@@ -0,0 +1,12 @@
+import * as nb from './numbers.js';
+import Product from './product.js';
+
+const x = nb.sum(3, 2);
+
+console.log(x);
+
+console.log(nb.exp(2, 3));
+
+const obj = new Product("Celular", 3250.99, 1);
+
+console.log(obj);
